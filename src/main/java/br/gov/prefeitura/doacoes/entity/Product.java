24d72d60c3314b1a38803f14ccedc52b15e70b26
Dev Team <dev@prefeitura.gov.br>
2026-07-08 02:00:47 +0000

    feat: create product entity

---
+++ src/main/java/br/gov/prefeitura/doacoes/entity/Product.java
@@ -0,0 +1,43 @@
+package br.gov.prefeitura.doacoes.entity;
+
+import br.gov.prefeitura.doacoes.entity.enums.ProductCategory;
+import br.gov.prefeitura.doacoes.entity.enums.ProductUnit;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "products")
+public class Product extends BaseEntity {
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private ProductCategory category;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private ProductUnit unit;
+
+}
