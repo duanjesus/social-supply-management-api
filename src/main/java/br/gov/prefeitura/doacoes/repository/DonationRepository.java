1ae7cde997bba9c5b9a66d32e45bec739cd2d709
Dev Team <dev@prefeitura.gov.br>
2026-07-08 02:00:53 +0000

    feat: create donation repository

---
+++ src/main/java/br/gov/prefeitura/doacoes/repository/DonationRepository.java
@@ -0,0 +1,9 @@
+package br.gov.prefeitura.doacoes.repository;
+
+import br.gov.prefeitura.doacoes.entity.Donation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DonationRepository extends JpaRepository<Donation, Long> {
+}
