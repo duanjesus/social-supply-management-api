397dd2453d8108e865049becb26c86fc6340cdd1
Dev Team <dev@prefeitura.gov.br>
2026-07-08 02:00:47 +0000

    feat: create donation entity

---
+++ src/main/java/br/gov/prefeitura/doacoes/entity/Donation.java
@@ -0,0 +1,50 @@
+package br.gov.prefeitura.doacoes.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "donations")
+public class Donation extends BaseEntity {
+
+    @Column(name = "donor_name", nullable = false, length = 150)
+    private String donorName;
+
+    @Column(name = "donor_document", length = 20)
+    private String donorDocument;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false, precision = 12, scale = 3)
+    private BigDecimal quantity;
+
+    @Column(name = "donation_date", nullable = false)
+    private LocalDate donationDate;
+
+    @Column(length = 500)
+    private String description;
+
+}
