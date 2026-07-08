d1a914c405ab033f9ca651e0fa61f81fa4a3d8ed
Dev Team <dev@prefeitura.gov.br>
2026-07-08 02:00:53 +0000

    feat: create product repository

---
+++ src/main/java/br/gov/prefeitura/doacoes/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package br.gov.prefeitura.doacoes.repository;
+
+import br.gov.prefeitura.doacoes.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    boolean existsByNameIgnoreCase(String name);
+
+}
