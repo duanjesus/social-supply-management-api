8ca332f66e090bbb0a5d62928262b5c00904645d
Dev Team <dev@prefeitura.gov.br>
2026-07-08 02:00:47 +0000

    feat: create distribution entity

---
+++ src/main/java/br/gov/prefeitura/doacoes/entity/Distribution.java
@@ -0,0 +1,51 @@
+package br.gov.prefeitura.doacoes.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "distributions")
+public class Distribution extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "institution_id", nullable = false)
+    private Institution institution;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false, precision = 12, scale = 3)
+    private BigDecimal quantity;
+
+    @Column(name = "distribution_date", nullable = false)
+    private LocalDate distributionDate;
+
+    @Column(name = "responsible_name", length = 150)
+    private String responsibleName;
+
+    @Column(length = 500)
+    private String observation;
+
+}
