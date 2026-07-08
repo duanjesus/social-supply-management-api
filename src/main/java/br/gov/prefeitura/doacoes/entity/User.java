0c263c5e9c64b348f3e169c5243cdeef86c6b556
Dev Team <dev@prefeitura.gov.br>
2026-07-08 02:00:47 +0000

    feat: create user entity for future authentication

---
+++ src/main/java/br/gov/prefeitura/doacoes/entity/User.java
@@ -0,0 +1,47 @@
+package br.gov.prefeitura.doacoes.entity;
+
+import br.gov.prefeitura.doacoes.entity.enums.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString(callSuper = true, exclude = "password")
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    @JsonIgnore
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private UserRole role;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Boolean active = true;
+
+}
