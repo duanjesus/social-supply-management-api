70efb451f58b8286d4cf8a6935113a44281a3b0a
Dev Team <dev@prefeitura.gov.br>
2026-07-08 02:00:53 +0000

    feat: create distribution repository

---
+++ src/main/java/br/gov/prefeitura/doacoes/repository/DistributionRepository.java
@@ -0,0 +1,9 @@
+package br.gov.prefeitura.doacoes.repository;
+
+import br.gov.prefeitura.doacoes.entity.Distribution;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DistributionRepository extends JpaRepository<Distribution, Long> {
+}
