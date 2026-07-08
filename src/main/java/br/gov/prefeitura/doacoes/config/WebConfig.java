3c4bbb30ee55281df1cfe6e0eb7994f2ae802dcd
Dev Team <dev@prefeitura.gov.br>
2026-07-08 02:01:06 +0000

    feat: configure cors policy

---
+++ src/main/java/br/gov/prefeitura/doacoes/config/WebConfig.java
@@ -0,0 +1,18 @@
+package br.gov.prefeitura.doacoes.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+
+}
